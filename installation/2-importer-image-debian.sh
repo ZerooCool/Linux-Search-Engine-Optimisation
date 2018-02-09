c347ed72c76fdc1ad20c82dde219df6439748c04
Bernard <g2pc@visionduweb.com>
2018-02-10 00:06:36 +0100

    Update 2-importer-image-debian.sh

--- installation/2-importer-image-debian.sh
+++ installation/2-importer-image-debian.sh
@@ -2,27 +2,35 @@
 echo "  "
 echo "  "
 
-echo " ----- Démarrage de l'installation ----- "
+echo " -- Démarrage de l'installation -- "
 
 echo "  "
-echo "  "
 
-echo " ----- Importation de l'image Docker Debian ----- "
+echo " -- Importation de l'image Docker Debian -- "
 docker pull debian
-echo " ----- Importation de l'image Docker Debian terminée ----- "
+echo " -- Importation de l'image Docker Debian terminée -- "
+
+echo "  "
+echo " -- Lire attentivement les commandes suivantes -- "
+echo " -- Appuyer la touche <Entrée> pour continuer -- "
+read touche
+case $touche in
+*)	echo " -- Installer le serveur web local dans le conteneur -- "
+	;;
+esac
+echo "  "
+
+echo " -- Vous êtes dans le shell du conteneur Docker Debian -- "
+echo " -- Le port 80 est mis en écoute du réseau -- "
+echo " -- Effectuer les étapes 1) 2) 3) 4) 5) -- "
 
 echo "  "
 echo "  "
 
-echo " ----- Installer le serveur web dans le conteneur Docker ----- "
-echo " 1) Installer wget : apt install wget"
-echo " 2) wget https://github.com/ZerooCool/Linux-Search-Engine-Optimisation/blob/master/installation/3-installer-le-serveur-dans-le-conteneur.sh"
-echo " 3) Lancer le script : sh 3-installer-le-serveur-dans-le-conteneur.sh"
+echo " 1) Mettre à jour les dépôts : apt update"
+echo " 2) Installer wget : apt install wget -y"
+echo " 3) wget https://github.com/ZerooCool/Linux-Search-Engine-Optimisation/blob/master/installation/3-installer-le-serveur-dans-le-conteneur.sh"
+echo " 4) Lancer le script : sh 3-installer-le-serveur-dans-le-conteneur.sh"
+echo " 5) Supprimer le script : rm 3-installer-le-serveur-dans-le-conteneur.sh"
 
-echo "  "
-echo "  "
-
-echo " ----- Entrée dans le shell du conteneur Docker Debian ----- "
-echo " ----- Le port 80 est mis en écoute du réseau ----- "
-echo " ----- Effectuer les étapes 1 , 2 , 3 ----- "
 docker run --name Debian -p 80:80 -t -i debian /bin/bash
