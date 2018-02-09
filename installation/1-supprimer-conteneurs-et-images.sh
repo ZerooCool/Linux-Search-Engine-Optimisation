9b999202a898d561a948871a05aeda67475d6a9c
Bernard <g2pc@visionduweb.com>
2018-02-09 23:41:44 +0100

    Update 1-supprimer-conteneurs-et-images.sh

--- installation/1-supprimer-conteneurs-et-images.sh
+++ installation/1-supprimer-conteneurs-et-images.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+echo "  "
+echo "  "
+
+echo " ----- Arrêt des conteneurs ----- "
 docker stop $(docker ps -a -q)
+echo " ----- Suppression des conteneurs ----- "
 docker rm $(docker ps -a -q)
+echo " ----- Suppression des images ----- "
 docker rmi $(docker images -q)
