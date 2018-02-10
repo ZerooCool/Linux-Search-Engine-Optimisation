faa0da904d0221f2f6e7ab8152b0a1a9f9785766
Bernard <g2pc@visionduweb.com>
2018-02-10 22:35:22 +0100

    Update 1-supprimer-conteneurs-et-images.sh

--- installation/1-supprimer-conteneurs-et-images.sh
+++ installation/1-supprimer-conteneurs-et-images.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
 echo "  "
-echo "  "
-
 echo " ----- Arrêt des conteneurs ----- "
 docker stop $(docker ps -a -q)
+sleep 4s
+echo "  "
 echo " ----- Suppression des conteneurs ----- "
 docker rm $(docker ps -a -q)
+sleep 4s
+echo "  "
 echo " ----- Suppression des images ----- "
 docker rmi $(docker images -q)
+echo "  "
+sleep 4s
+clear
+echo " ----- Les conteneurs et les images n'existent plus ----- "
+sleep 4s
+echo " ----- Importer une nouvelle image pour utiliser Docker ----- "
+sleep 4s
+echo "  "
+echo " ----- Conseil ----- "
+sleep 4s
+echo " ----- Pour importer une nouvelle image Docker ----- "
+sleep 4s
+echo " ----- Lancer la commande suivante dans le terminal ----- "
+sleep 4s
+echo "  "
+echo " sh 2-importer-image-debian.sh "
+sleep 4s
+echo "  "
+echo " ----- Vous ne voulez pas importer une nouvelle image Docker Debian ? ----- "
+sleep 4s
+echo " ----- Vous avez terminé et pouvez quitter le terminal ! ----- "
+sleep 4s
