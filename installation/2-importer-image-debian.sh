b209ab2d2746c2d7be903dea121489d5b74d7187
Bernard <g2pc@visionduweb.com>
2018-02-10 22:12:00 +0100

    Update 2-importer-image-debian.sh

--- installation/2-importer-image-debian.sh
+++ installation/2-importer-image-debian.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
 echo "  "
-echo "  "
-
 echo " -- Démarrage de l'installation -- "
+sleep 4s
 
 echo "  "
-
 echo " -- Importation de l'image Docker Debian -- "
+echo "  "
 docker pull debian
+echo "  "
 echo " -- Importation de l'image Docker Debian terminée -- "
+sleep 4s
 
 echo "  "
-echo " -- Noter les commandes suivantes. Elles vont installer le serveur local. -- "
-echo " -- Appuyer la touche <Entrée> pour continuer -- "
-read touche
-case $touche in
-*)	echo " -- Installer le serveur web local dans le conteneur -- "
-	;;
-esac
+echo " -- Noter les commandes suivantes -- "
+echo " -- Vous devez les lancer vous même pour installer le serveur local -- "
+sleep 4s
 echo "  "
-
 echo " -- Vous êtes dans le shell du conteneur Docker Debian -- "
 echo " -- Le port 80 est mis en écoute du réseau -- "
-echo " -- Effectuer les étapes 1) 2) 3) 4) 5) -- "
+echo " -- Effectuer les étapes de 1 à 5 -- "
+sleep 4s
 
 echo "  "
-echo "  "
-
 echo " 1) Mettre à jour les dépôts : apt update"
+sleep 2s
 echo " 2) Installer wget : apt install wget -y"
-echo " 3) Aller dans le bon dossier : cd /home "
+sleep 2s
+echo " 3) Aller dans le dossier : cd /home "
+sleep 2s
 echo " 4) wget https://github.com/ZerooCool/Linux-Search-Engine-Optimisation/raw/master/installation/3-installer-le-serveur-dans-le-conteneur.sh"
+sleep 2s
 echo " 5) Lancer le script : sh /home/3-installer-le-serveur-dans-le-conteneur.sh"
+sleep 2s
+echo "  "
 
 docker run --name Debian -p 80:80 -t -i debian /bin/bash
