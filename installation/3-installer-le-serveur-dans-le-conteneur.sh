0135cabcd82d84d67f36a1ae2cef6714a00b4668
Bernard <g2pc@visionduweb.com>
2018-02-10 21:19:11 +0100

    Update 3-installer-le-serveur-dans-le-conteneur.sh

--- installation/3-installer-le-serveur-dans-le-conteneur.sh
+++ installation/3-installer-le-serveur-dans-le-conteneur.sh
@@ -56,11 +56,16 @@ cd /var/www/html/hello
 echo " "
 echo " "
 
+# Importer le fichier index.php et l'image seo. Permet d'avoir accès aux scripts directement.
+echo "Importer le fichier index.php et l'image seo. Permet d'avoir accès aux scripts directement."
+wget https://raw.githubusercontent.com/ZerooCool/Linux-Search-Engine-Optimisation/master/hello/index.php
+wget https://github.com/ZerooCool/Linux-Search-Engine-Optimisation/raw/master/hello/seo.jpg
+
 # Créer le fichier hello.php pour vérifier le fonctionnement de PHP.
 echo "Créer le fichier hello.php pour vérifier le fonctionnement de PHP."
 echo "<?php" >> hello.php
 echo "echo 'SEO';" >> hello.php
-echo "?>" >> hello.phpn
+echo "?>" >> hello.php
 
 # Créer le fichier phpinfo.php pour obtenir les informations du serveur.
 echo "Créer le fichier phpinfo.php pour obtenir les informations du serveur."
