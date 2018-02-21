3d4460e78741116ff3452b09c405aac2ee1c8440
Bernard <g2pc@visionduweb.com>
2018-02-21 01:09:51 +0100

    Update 00-menu.sh

--- installation/00-menu.sh
+++ installation/00-menu.sh
@@ -278,10 +278,10 @@ sleep 4
   echo " Supprimer Docker "
   sleep 4
 
-cd /home/
+cd ~/installeur/
 curl https://raw.githubusercontent.com/ZerooCool/Linux-Search-Engine-Optimisation/master/installation/1-supprimer-docker.sh > ~/installeur/1-supprimer-docker.sh
 sh 1-supprimer-docker.sh
-cd /home/
+cd ~/installeur/
 rm 1-supprimer-docker.sh
 sleep 4
   ;;
