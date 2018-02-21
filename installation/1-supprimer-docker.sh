9f04f7e9cbc8a25a9472ffbc922c538996db3a50
Bernard <g2pc@visionduweb.com>
2018-02-21 01:05:08 +0100

    Update 1-supprimer-docker.sh

--- installation/1-supprimer-docker.sh
+++ installation/1-supprimer-docker.sh
@@ -1,60 +1,177 @@
 #!/bin/bash
+# En root.
 
-apt autoremove docker docker-engine docker-compose
-if [ "$?" = "0" ] ; then
-echo "Docker a été supprimé."
+# Support sur Github.
+echo
+echo "${rougefonce}En cas d'erreur durant l'installation, merci d'ouvrir une issue depuis Github${neutre}"
+echo "https://github.com/ZerooCool/Linux-Search-Engine-Optimisation/issues"
+echo
+sleep 4
+
+###########
+########### Vérifier si le dossier d'installation existe, sinon, le créer.
+ABS_PATH_INSTALLEUR=$(readlink -e ~/installeur)
+if [ -d "$ABS_PATH_INSTALLEUR" ]; then
+ echo " Initialisation - Le dossier d'installation existe "
+ sleep 3
+# Logs.txt
+cd ~/installeur
+echo "$jour - $heure : Initialisation - Le dossier d'installation existe." >> logs.txt
 else
-echo "Docker n'a pas été supprimé."
+ echo " Chargement - Le dossier d'installation n'existe pas et va être créé "
+ sleep 3
+ mkdir ~/installeur
+# Logs.txt
+cd ~/installeur
+echo "$jour - $heure : Chargement - Le dossier d'installation n'existe pas et va être créé." >> logs.txt
+fi
+###########
+###########
+
+apt purge docker-engine docker-compose
+if [ "$?" = "0" ] ; then
+echo "Les paquets de Docker ne sont plus installés"
+# Logs.txt
+cd ~/installeur
+echo "$jour - $heure : Les paquets de Docker ne sont plus installés." >> logs.txt
+else
+echo "Les paquets de Docker n'ont pas été supprimés"
+# Logs.txt
+cd ~/installeur
+echo "$jour - $heure : Les paquets de Docker n'ont pas été supprimés." >> logs.txt
 fi
 sleep 4
 
-apt autoremove dirmngr
-if [ "$?" = "0" ] ; then
-echo "Le paquet dirmngr pour gérer les clés est supprimé."
-else
-echo "Le paquet dirmngr pour gérer les clés n'a pas été supprimé."
-fi
-sleep 4
+# Dirmngr est utilisé pour l’accès au réseau par gpg, gpgsm et dirmngr-client entre autres outils.
+# À moins que ce paquet ne soit installé, les composants de la suite GnuPG essayant d’interagir avec le réseau échoueront. 
+# apt autoremove dirmngr
+# if [ "$?" = "0" ] ; then
+# echo "Le paquet dirmngr pour gérer les clés est supprimé."
+# else
+# echo "Le paquet dirmngr pour gérer les clés n'a pas été supprimé."
+# fi
+# sleep 4
 
 apt autoremove
 if [ "$?" = "0" ] ; then
-echo "Les paquets qui ne sont plus nécessaires ont également été supprimés."
+echo "Les paquets qui ne sont plus nécessaires ont été supprimés"
+# Logs.txt
+cd ~/installeur
+echo "$jour - $heure : Les paquets qui ne sont plus nécessaires ont été supprimés." >> logs.txt
 else
-echo "Les paquets qui ne sont plus nécessaires n'ont pas été supprimé."
+echo "Les paquets qui ne sont plus nécessaires n'ont pas été supprimé"
+# Logs.txt
+cd ~/installeur
+echo "$jour - $heure : Les paquets qui ne sont plus nécessaires n'ont pas été supprimés." >> logs.txt
+fi
+sleep 4
+
+
+# Vérifier si le dossier /var/lib/docker existe, si oui, le supprimer.
+ABS_PATH_INSTALLEUR=$(readlink -e /var/lib/docker)
+if [ -d "$ABS_PATH_INSTALLEUR" ]; then
+ echo " Le dossier /var/lib/docker existe. Il va être supprimé. "
+ sleep 3
+rm -rf /var/lib/docker
+if [ "$?" = "0" ] ; then
+echo "Le dossier /var/lib/docker a été supprimé"
+# Logs.txt
+cd ~/installeur
+echo "$jour - $heure : Le dossier /var/lib/docker a été supprimé." >> logs.txt
+else
+echo "Le dossier /var/lib/docker n'a pas été supprimé correctement"
+# Logs.txt
+cd ~/installeur
+echo "$jour - $heure : Le dossier /var/lib/docker n'a pas été supprimé correctement." >> logs.txt
+fi
+sleep 4
+
+else
+ echo " Le dossier /var/lib/docker n'existe pas. Il a déjà été supprimé. "
+ sleep 3
+fi
+
+
+# Supprimer la source du dépôt Docker
+if [ -f "menu.sh" ]; then
+echo " Le fichier du menu existe "
+sleep 3
+
+rm /etc/apt/sources.list.d/docker.list
+if [ "$?" = "0" ] ; then
+echo "Le dépôt ajouté pour Docker a été supprimé"
+# Logs.txt
+cd ~/installeur
+echo "$jour - $heure : Le dépôt ajouté pour Docker a été supprimé." >> logs.txt
+else
+echo "Le dépôt ajouté pour Docker n'a pas été supprimé correctement"
+# Logs.txt
+cd ~/installeur
+echo "$jour - $heure : Le dépôt ajouté pour Docker n'a pas été supprimé correctement." >> logs.txt
+fi
+sleep 4
+
+else
+echo " Le dépôt ajouté pour Docker a déjà été supprimé "
+# Logs.txt
+cd ~/installeur
+echo "$jour - $heure : Le dépôt ajouté pour Docker a déjà été supprimé." >> logs.txt
+sleep 3
+fi
+
+
+apt-key del 2C52609D
+if [ "$?" = "0" ] ; then
+echo "Toujours OK. La clé de dépôt ajoutée pour Docker a été supprimée"
+# Logs.txt
+cd ~/installeur
+echo "$jour - $heure : Toujours OK. La clé de dépôt ajoutée pour Docker a été supprimée." >> logs.txt
+else
+echo "Toujours OK. La clé de dépôt ajoutée pour Docker n'a pas été supprimée"
+# Logs.txt
+cd ~/installeur
+echo "$jour - $heure : Toujours OK. La clé de dépôt ajoutée pour Docker n'a pas été supprimée." >> logs.txt
 fi
 sleep 4
 
 apt clean
 if [ "$?" = "0" ] ; then
-echo "Les paquets présents dans /var/cache/apt/archives ont été supprimés."
+echo "Les paquets présents dans /var/cache/apt/archives ont été supprimés"
+# Logs.txt
+cd ~/installeur
+echo "$jour - $heure : Les paquets présents dans /var/cache/apt/archives ont été supprimés." >> logs.txt
 else
-echo "Les paquets présents dans /var/cache/apt/archives n'ont pas été supprimés."
+echo "Les paquets présents dans /var/cache/apt/archives n'ont pas été supprimés"
+# Logs.txt
+cd ~/installeur
+echo "$jour - $heure : Les paquets présents dans /var/cache/apt/archives n'ont pas été supprimés." >> logs.txt
 fi
 sleep 4
 
-apt purge '~c'
-if [ "$?" = "0" ] ; then
-echo "Les fichiers de configuration des paquets désinstallés non purgés ont été supprimés."
-else
-echo "Les fichiers de configuration des paquets désinstallés non purgés n'ont pas été supprimés."
-fi
-sleep 4
-
-rm /etc/apt/sources.list.d/docker.list
-if [ "$?" = "0" ] ; then
-echo "Le dépôt ajouté pour Docker a été supprimé."
-else
-echo "Le dépôt ajouté pour Docker n'a pas été supprimé."
-fi
-sleep 4
+# aptitude purge '~c'
+## aptitude not found. N'est pas installé par défaut.
+# if [ "$?" = "0" ] ; then
+# echo "Les fichiers de configuration des paquets désinstallés non purgés ont été supprimés"
+# Logs.txt
+# cd ~/installeur
+# echo "$jour - $heure : Les fichiers de configuration des paquets désinstallés non purgés ont été supprimés." >> logs.txt
+# else
+# echo "Les fichiers de configuration des paquets désinstallés non purgés n'ont pas été supprimés"
+# Logs.txt
+# cd ~/installeur
+# echo "$jour - $heure : Les fichiers de configuration des paquets désinstallés non purgés n'ont pas été supprimés." >> logs.txt
+# fi
+# sleep 4
 
 echo
 echo "Le processus de suppression de Docker est terminé"
-echo "En cas d'erreur durant l'installation, merci d'ouvrir une issue depuis GitHub."
-echo "https://github.com/ZerooCool/Linux-Search-Engine-Optimisation/issues"
+# Logs.txt
+cd ~/installeur
+echo "$jour - $heure : Le processus de suppression de Docker est terminé." >> logs.txt
 sleep 4
 
-# Faire revenir sur le menu.
-# placer le script ...
-# charger le script ...
-# lancer le script ...
+# Revenir sur le menu.
+# Logs.txt
+cd ~/installeur
+echo "$jour - $heure : Revenir sur le menu." >> logs.txt
+sh ~/installeur/00-menu.sh
