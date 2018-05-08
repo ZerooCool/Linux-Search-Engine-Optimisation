840666a8acb58d792321d032807a424bffbf991c
Zer00CooL <g2pc@visionduweb.com>
2018-05-08 18:11:08 +0200

    chmod et chown le fichier phpinfo.php
    Ce fichier n'a pas vocation a être lisible sur un site en production.

--- hello/phpinfo.php
+++ hello/phpinfo.php
@@ -1,3 +1,18 @@
+# Le fichier phpinfo.php informe l'administrateur système des paquets installés sur le serveur.
+# Ce fichier n'a pas vocation a être lisible sur un site en production.
+
+# Interdire l'accès à ce fichier quand il n'est pas nécessaire à l'administrateur système.
+# Donner le fichier à l'utilisateur root.
+# sudo chown root:root phpinfo.php
+# Supprimer les droits d'accès.
+# sudo chmod 0000 phpinfo.php
+
+# Autoriser l'accès à ce fichier quand il est nécessaire à l'administrateur système.
+# Donner le fichier à l'utilisateur www-data.
+# sudo chown www-data:www-data phpinfo.php
+# Autoriser les droits en lecture.
+# sudo chmod 0444 phpinfo.php
+
 <?php
 phpinfo();
 ?>
