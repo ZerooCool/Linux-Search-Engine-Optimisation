c6b86c7bac4d1d83e0da1dadc102fe586f8ca24a
Zer00CooL <g2pc@visionduweb.com>
2018-09-15 23:05:57 +0200

    Mise à jour pour la version de Joomla 3.8.12.1
    https://github.com/AFUJ/joomla-cms-fr/releases/download/3.8.12.1/Joomla_3.8.12-FR-Stable-Full_Package.zip

--- installation/4-installer-joomla.sh
+++ installation/4-installer-joomla.sh
@@ -9,16 +9,16 @@ echo "Se placer à la racine du dossier /var/www/html/ et charger Joomla!"
 cd /var/www/html/
 
 echo " Rapatrier la dernière version de Joomla! "
-# Rapatrier Joomla! 3.8.7 qui est la dernière version stable lors de la rédaction de ce tutoriel.
-wget https://github.com/AFUJ/joomla-cms-fr/releases/download/3.8.7.1/Joomla_3.8.7-FR-Stable-Full_Package.zip
+# Rapatrier Joomla! 3.8.12.1 qui est la dernière version stable lors de la rédaction de ce tutoriel.
+wget https://github.com/AFUJ/joomla-cms-fr/releases/download/3.8.12.1/Joomla_3.8.12-FR-Stable-Full_Package.zip
 
 echo " "
 
 # Dézipper l'archive zip au même niveau.
 echo "Dézipper Joomla! dans le répertoire courant /var/www/html/"
-unzip Joomla_3.8.7-FR-Stable-Full_Package.zip
+unzip Joomla_3.8.12.1-FR-Stable-Full_Package.zip
 # Supprimer l'archive zip.
-rm Joomla_3.8.7-FR-Stable-Full_Package.zip
+rm Joomla_3.8.12.1-FR-Stable-Full_Package.zip
 
 echo " "
 
